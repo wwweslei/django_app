927250d24f89af10830fcffa4d94cab947d90a6d
weslei <www.weslei@gmail.com>
2017-03-27 21:41:55 -0300

    configurações de seguraça

--- mysite/settings.py
+++ mysite/settings.py
@@ -2,19 +2,14 @@ import os
 from decouple import config
 from dj_database_url import parse as dburl
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
+#SECRET_KEY = 'fmae81&xxegm%e^6gd5-m_u=rewl!vvt*u79bf#(fepiy3j8#d'
+#DEBUG = True
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fmae81&xxegm%e^6gd5-m_u=rewl!vvt*u79bf#(fepiy3j8#d'
-DEBUG = True
-
-#SECRET_KEY = config('SECRET_KEY')
-#DEBUG = config('DEBUG', default=False, cast=bool)
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
---
+++ mysite/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('<h1>Em construção</h1><h3>Trabalhando !!!!!!</h3>')
